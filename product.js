7dcc546d1c4e73c2307849113a488595f450a661
selvinjeba <selvinpaul78@gmail.com>
2023-12-16 02:59:44 +0000

    updated product.js

--- product.js
+++ product.js
@@ -29,18 +29,21 @@ search.addEventListener("keyup", function(){
 
     var enteredvalue = event.target.value.toUpperCase();
 
-    for(count=0;count<productlist.length;count=count+1);
+    for(count=0; count<productlist.length; count=count+1)
+    {
 
-    var productname = productlist[count].querySelector("h1").textContent
+       var productname = productlist[count].querySelector("h1").textContent;
     
-    if(productname[count].textContent.toUpperCase.indexOf(enteredvalue)<0){
+       if(productname.toUpperCase().indexOf(enteredvalue)<0){
 
         productlist[count].style.display = "none";
     }
 
-    else{
+        else{
 
-        productlist[count].style.display = "block";
+            productlist[count].style.display = "block";
+
+       }
 
     }
-})
+}) 
